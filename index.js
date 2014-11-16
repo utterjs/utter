c3e94c6893a151cf85f86925abca9e1ff06a4949
Luke Evers <me@lukevers.com>
2014-11-16 18:56:11 -0500

    Rework channel.js and server.js, and create fake data in index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// For now just load and render the ui
-require('./ui/window.js').render();
+// Require server/channel
+var server  = require('./lib/server.js');
+var channel = require('./lib/channel.js');
+
+// Create fake data for now
+var hypeirc = new server(); 
+hypeirc.channels.push(new channel());
+hypeirc.channels.push(new channel());
+hypeirc.channels[0].userlist = ['lukevers', 'ansuz'];
+hypeirc.channels[1].userlist = ['inhies', 'ansuz'];
+
+// Output to check that it's working
+console.log(hypeirc.channels);
+
+//require('./ui/window.js').render();
